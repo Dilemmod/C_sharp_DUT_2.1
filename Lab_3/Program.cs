442f58f1da81dc0309e09259ab33bee54ab8cd13
Dileon <yevtayev@gmail.com>
2019-09-23 19:43:44 +0300

    Done Lab_3 and Lab_4

---
+++ Lab_3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lab_3 
+            //6 Варiант:
+            //Перевірити iстинність вислову: "Данне ціле число є непарним тризначним числом
+            Console.Write("Введiть цiле число яке є непарним тризначним числом ");
+            string str = Console.ReadLine();
+            //Перевірка чи str число
+            int n;
+            bool isNum = int.TryParse(str, out n);
+
+            if (isNum)
+            {
+                //Перетворення str в int
+                n = Convert.ToInt32(str);
+                //Перевірка на парність
+                if (n % 2 != 0)
+                {
+                    //Чи є тризначни числом
+                    int i = 0;
+                    while (n > 0)
+                    {
+                        i++;
+                        n /= 10;
+                    }
+                    if (i == 3) Console.WriteLine("Данне цiле число є непарним тризначним числом");
+                    else Console.WriteLine("Не вiдповiдає умовi");
+
+                }
+                else Console.WriteLine("Не вiдповiдає умовi");
+            }
+            else Console.WriteLine("Не вiдповiдає умовi");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab_4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    class Program
+    {
+         
+        static void Main(string[] args)
+        {
+
+            //Lab_4
+            //Варіант_17
+            //Дано дійсне число R і масив розміру N. Знайти елемент масиву, який найближчий (найдальший) від даного числа.
+            Console.Write("Enter size masiv N "); int size = Convert.ToInt32(Console.ReadLine());
+            double[] mas = new double[size];
+            Console.Write("Enter the digit R "); double r = Convert.ToDouble(Console.ReadLine());
+            double temp = 0, max = double.MaxValue;
+
+            for (int i = 0; i < mas.Length; i++) 
+            {
+                Console.Write("Enter [" + (i + 1) + "] element ");
+                mas[i] = Convert.ToDouble(Console.ReadLine());
+                double a = Math.Abs(Math.Abs(mas[i]) - Math.Abs(r));
+                if (a < max)
+                {
+                    temp = mas[i];
+                    max = a;
+                }
+            }
+            Console.WriteLine("The closest number to R: "+temp);
+            Console.ReadKey();
+
+
+        }
+    }
+    
+}
