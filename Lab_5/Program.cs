3dfecebfa7bef1ae914ad29d582eaf39231d2818
Dileon <yevtayev@gmail.com>
2019-10-19 13:14:34 +0300

    Lab_5

--- Lab_5/Program.cs
+++ Lab_5/Program.cs
@@ -6,21 +6,9 @@ using System.Threading.Tasks;
 
 namespace Lab_5
 {
+    
     class Program
     {
-        public static void Arrey(int size1,int size2, int[,] mas)
-        {
-            Random r = new Random();
-            for (int i = 0; i < size1; i++)
-            {
-                for (int j = 0; j < size2; j++)
-                {
-                    mas[i, j] = r.Next(10);
-                    Console.Write(" " + mas[i, j]);
-                }
-                Console.WriteLine();
-            }
-        }
         static void Main(string[] args)
         {
             //Lab_5
@@ -28,15 +16,71 @@ namespace Lab_5
             //17. Дана квадратна матриця порядку M. Дзеркально відобразити її
             // елементи відносно 1) горизонтальної осі симетрії; 2) вертикальної
             // осі симетрії; 3) головної діагоналі; 4) побічної діагоналі матриці.
-            int  temp = 0;
-            Console.Write("Enter size masiv M "); int size = Convert.ToInt32(Console.ReadLine());
-            int[,] mas = new int[size, size];
+            Console.Write("Enter size masiv M ");
+            Array array = new Array(Convert.ToInt32(Console.ReadLine()));
 
-            Console.WriteLine("The original matrix");
-
-            //Arrey виводить масив
-            Arrey(size,size, mas);
-
+            Console.WriteLine("The original arrat");
+            array.initializationArray();
+            Console.WriteLine("Horizontal axis of symmetry");
+            array.horizontalMirrorArray();
+            Console.WriteLine("Vertical axis of symmetry");
+            array.verticalMirorArray();
+            Console.WriteLine("Miror array on the main diagonal");
+            array.diagonalMirorArray();
+            Console.WriteLine("Miror array on the side diagonal");
+            array.sideDiagonalArray();
+            Console.ReadKey();
+        }
+    }
+    class Array
+    {
+        private int temp;
+        private int size;
+        private int[,] mas;
+        private int[,] temporary;
+        public Array(int size)
+        {
+            mas = new int[size, size];
+            temporary = new int[size,size];
+            this.size = size;
+        }
+        public void getArray()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(" " + mas[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public void initializationArray()
+        {
+            Random r = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    temporary[i,j] = r.Next(10);
+                    mas[i, j] = temporary[i, j];
+                }
+            }
+            getArray();
+        }
+        private void reset()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    mas[i, j] = temporary[i, j];
+                }
+            }
+        }
+        public void horizontalMirrorArray()
+        {
+            reset();
             for (int i = 0; i < (size / 2); i++)
             {
                 for (int j = 0; j < size; j++)
@@ -46,13 +90,51 @@ namespace Lab_5
                     mas[(size - i - 1), j] = temp;
                 }
             }
-            Console.WriteLine();
-
-            Console.WriteLine("Mirror matrix");
-
-            Arrey(size/2,size, mas);
-
-            Console.ReadKey();
+            getArray();
+        }
+        public void verticalMirorArray()
+        {
+            reset();
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < (size/2); j++)
+                {
+                    temp = mas[i, j];
+                    mas[i, j] = mas[i, (size - j - 1)];
+                    mas[i, (size - j - 1)] = temp;
+                }
+            }
+            getArray();
+        }
+        public void diagonalMirorArray()
+        {
+            reset();
+            for (int i = size-1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++) 
+                {
+                    temp = mas[i, j];
+                    mas[i, j] = mas[j, i];
+                    mas[j, i] = temp;
+                }
+            }
+            getArray();
+        }
+        public void sideDiagonalArray()
+        {
+            reset();
+            for (int i = 0; i < (size-1); i++)
+            {
+                for (int j = 0; j < (size - i -1); j++)
+                {
+                    temp = mas[i, j];
+                    mas[i, j] = mas[(size-1-j),(size -1 - i)];
+                    mas[(size - 1 - j),(size - 1 - i)] = temp;
+                }
+            }
+            getArray();
         }
     }
 }
+    
+
---
+++ Lab_6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            Console.WriteLine("++i = " + (++i));//6
+            Console.WriteLine("i++ = " + (i++));//7
+            Console.WriteLine("++i = " + (++i));//7
+            Console.WriteLine("++i = " + (++i));//8
+            Console.WriteLine("--i = " + (i--));//7
+            Console.WriteLine("++i = " + (++i));//8
+
+            Console.ReadKey();
+        }
+    }
+}
