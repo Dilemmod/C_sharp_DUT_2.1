0c82d56700348e44ab682efc7119d4a5961f4de5
Dileon <yevtayev@gmail.com>
2019-10-21 14:28:13 +0300

    Lab_6

--- Lab_6/Program.cs
+++ Lab_6/Program.cs
@@ -10,13 +10,45 @@ namespace Lab_6
     {
         static void Main(string[] args)
         {
-            int i = 5;
-            Console.WriteLine("++i = " + (++i));//6
-            Console.WriteLine("i++ = " + (i++));//7
-            Console.WriteLine("++i = " + (++i));//7
-            Console.WriteLine("++i = " + (++i));//8
-            Console.WriteLine("--i = " + (i--));//7
-            Console.WriteLine("++i = " + (++i));//8
+            // Lab 6
+            // Варіант 6.
+            // Створити ліст інтових змінних, який може вміщати тільки 1 і 0,
+            // заповнити його з клавіатури. Порахувати кількість 1 і 0.
+            // Скопіювати отриманий ліст в масив.
+            List<int> oneOrZero = new List<int>();
+            ConsoleKeyInfo exit;
+            int zeros = 0, units = 0;
+            Console.WriteLine("Enter 0 or 1 : ");
+            Console.WriteLine("Enter ENTER key for exit ");
+            do
+            {
+                exit = Console.ReadKey(true);
+                switch (exit.Key)
+                {
+                    case ConsoleKey.D0:
+                        zeros++;
+                        oneOrZero.Add(0);
+                        Console.Write("0 ");
+                        break;
+                    case ConsoleKey.D1:
+                        units++;
+                        oneOrZero.Add(1);
+                        Console.Write("1 ");
+                        break;
+                }
+            } while (exit.Key != ConsoleKey.Enter);
+            Console.WriteLine();
+
+            Console.WriteLine("The array to which was copied list = ");
+            int[] copyList = oneOrZero.ToArray();
+            for (int i = 0; i < copyList.Length; i++)
+            {
+                Console.Write(copyList[i] +" ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Number of units = " + units);
+            Console.WriteLine("Number of zeros = " + zeros);
 
             Console.ReadKey();
         }
